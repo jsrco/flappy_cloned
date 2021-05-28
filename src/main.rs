c5a862c2a15927405afd7e1aca2ec4e5a02e56ba
Ryan Orlando <jsrco@hotmail.com>
2021-05-27 22:05:42 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,16 @@ impl State {
             mode: GameMode::Menu,
         }
     }
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+    }
     fn play(&mut self, ctx: &mut BTerm) { // TODO: Fill in this stub later self.mode = GameMode::End;
+    }
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
     }
 }
 
